2059a7f5922f335bdbd0648a04615aa6974fdfb7
Tomasz Gałaj <tomaszgalaj@gmail.com>
2018-08-13 15:13:58 +0200

    Travis script update

--- script/jekyll-build.sh
+++ script/jekyll-build.sh
@@ -17,12 +17,11 @@ mkdir _site
 git clone https://${GH_TOKEN}@github.com/Shot511/shot511.github.io.git --branch master _site
 
 # build with Jekyll into "_site"
-bundle exec jekyll build
+# exec jekyll build
 
 # push
 cd _site
 git config user.email "tomasz.galaj@gmail.com"
 git config user.name "Shot511"
-git add --all
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git commit -a -m "rebuild pages" --allow-empty
 git push -u origin master
